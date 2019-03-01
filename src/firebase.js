30bdbabe14218390fd6273b118fea73e3df8e400
AlexxMaker <Alexeus92@mail.ru>
2019-03-01 22:51:00 +0430

    Firebase connected

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+const config = {
+    apiKey: "AIzaSyCgC5SrH2F-c_okBdHBGptRM9KkFdnoh68",
+    authDomain: "m-city-6a129.firebaseapp.com",
+    databaseURL: "https://m-city-6a129.firebaseio.com",
+    projectId: "m-city-6a129",
+    storageBucket: "m-city-6a129.appspot.com",
+    messagingSenderId: "327788633502"
+  };
+
+  firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database();
+
+  firebaseDB.ref('matches').once('value').then((snapshot)=>{
+    console.log(snapshot.val());
+  })
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes'
 import './Resources/css/app.css'
+import './firebase'
 
 import { BrowserRouter } from 'react-router-dom'
 
