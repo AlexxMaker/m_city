65263a918f0f9b93cf1812b278a37e2fa131c45e
AlexxMaker <Alexeus92@mail.ru>
2019-03-08 18:40:56 +0430

    Working on EditMatch forms 2

--- src/routes.js
+++ src/routes.js
@@ -9,14 +9,16 @@ import PublicRoute from './Components/authRoutes/publicRoutes'
 
 
 //Admin routes
-import Dashboard from './Components/Admin/Dashboard'
+import Dashboard from './Components/Admin/Dashboard';
 import AdminMatches from './Components/Admin/matches';
+import AddEditMatch from './Components/Admin/matches/addEditMatch';
 
 const Routes = (props) => {
 
   return (
       <Layout>
         <Switch>
+          <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
           <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
           <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
           <PublicRoute {...props} path="/sign_in" restricted={true} exact component={SignIn}/>
